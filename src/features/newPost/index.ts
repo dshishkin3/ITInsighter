da4aa92ecd8b8d0717aa1f70dafec5b0f74fda2c
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2024-02-09 15:12:28 +0300

    feature/BL-2_new-post

--- src/features/newPost/index.ts
+++ src/features/newPost/index.ts
@@ -1,3 +1,2 @@
-// импортируем в новый index.ts или в уже существуюший в searchInput?
 import { NewPost } from "./ui/NewPost";
 export { NewPost };
--- src/features/newPost/ui/NewPost.tsx
+++ src/features/newPost/ui/NewPost.tsx
@@ -1,15 +1,16 @@
 "use client";
-import { Box, Button, Divider } from "@mantine/core";
-import styles from "./styles.module.scss";
+
+import React from "react";
+import { Button } from "@mantine/core";
 import { IconPencil } from "@tabler/icons-react";
 import Link from "next/link";
+
+import styles from "./styles.module.scss";
 export function NewPost() {
-    const [open, setOpen] = React.useState<boolean>(false);
     return (
         <>
             <Link href="/editor">
                 <Button
-                    onClick={() => setOpen(!open)}
                     className={styles.new__post}
                     leftSection={<IconPencil />}
                 >
@@ -19,22 +20,3 @@ export function NewPost() {
         </>
     );
 }
-// Импортировать компонент в Header
-import React, { useState } from "react";
-
-const PopupButton: React.FC = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-
-    const togglePopup = () => {
-        setIsOpen(!isOpen);
-    };
-
-    return (
-        <div>
-            <button onClick={togglePopup}>Открыть попап</button>
-            {isOpen && <div>Содержимое попапа</div>}
-        </div>
-    );
-};
-
-export default PopupButton;
